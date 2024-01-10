10fb944dce7f988abf29991c0ddabf93a1c50052
Moritz Ungemach <Moritz.Ungemach@flyeralarm.com>
2024-01-10 18:04:09 +0100

    .

--- mustergenerator.py
+++ mustergenerator.py
@@ -1,18 +1,69 @@
-def letzte_zahl_im_muster(anzahl):
-    result = []
-    current = 0
+def generate_sequence(o):
+    if(o==1):
+        return 0
+    elif(o==2):
+        return 1
+    elif(o==3):
+        return 0
+    elif(o==4):
+        return 1
+    elif(o==5):
+        return 2
+    elif(o==6):
+        return 1    
+    elif(o==7):
+        return 2
+    elif(o==8):
+        return 3
+    elif(o==9):
+        return 2
+    elif(o==10):
+        return 3
+    elif(o==11):
+        return 4
+    elif(o==12):
+        return 3
+    elif(o==13):
+        return 4
+    elif(o==14):
+        return 5
+    elif(o==15):
+        return 6
+    elif(o==16):
+        return 7
+    elif(o==17):
+        return 8
+    elif(o==18):
+        return 7
+    elif(o==19):
+        return 8
+    elif(o==20):
+        return 9
+    elif(o==21):
+        return 10
+    elif(o==22):
+        return 11
+    elif(o==23):
+        return 12
+    elif(o==24):
+        return 11
+    elif(o==25):
+        return 12
+    elif(o==26):
+        return 13
+    elif(o==27):
+        return 14
+    elif(o==28):
+        return 15
+    elif(o==29):
+        return 16
+    elif(o==30):
+        return 17
+    elif(o==31):
+        return 16
+    elif(o==32):
+        return 17
+    elif(o==33):
+        return 1
 
-    for i in range(anzahl):
-        result.append(current)
-        current += 1
-
-        if i % 2 == 1:
-            current = result[i // 2] + 1
-
-    return result[-1] if result else None
-
-# Beispielaufruf mit einer Anzahl von 17
-anzahl = 3
-letzte_zahl = letzte_zahl_im_muster(anzahl)
-return letzte_zahl
 
